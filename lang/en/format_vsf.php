4dd3e1395f97dfc0d96ee499b19667b122429964
Gareth J Barnard <gjbarnard@gmail.com>
2016-04-08 13:39:05 +0100

    Lang!

---
+++ lang/en/format_vsf.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Progress Section Format
+ *
+ * @package    course/format
+ * @subpackage vsf
+ * @version    See the value of '$plugin->version' in version.php.
+ * @copyright  &copy; 2016-onwards G J Barnard in respect to modifications of standard topics format.
+ * @author     G J Barnard - gjbarnard at gmail dot com and {@link http://moodle.org/user/profile.php?id=442195}
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+ 
+$string['currentsection'] = 'This section';
+$string['deletesection'] = 'Delete section';
+$string['sectionname'] = 'Section';
+$string['pluginname'] = 'Progress Section Format';
+$string['section0name'] = 'General';
+$string['page-course-view-vsf'] = 'Any course main page in the progress section format';
+$string['page-course-view-vsf-x'] = 'Any course page in progress section format';
+$string['hidefromothers'] = 'Hide section';
+$string['showfromothers'] = 'Show section';
