aebe40e81aaee1c2d0f528257b29a64bbe44c705
Gareth J Barnard <gjbarnard@gmail.com>
2016-04-08 17:09:07 +0100

    FillDonut plugin for Chartist.js and style.

--- amd/src/vsf_chart.js
+++ amd/src/vsf_chart.js
@@ -12,47 +12,187 @@
 /* jshint ignore:start */
 define(['jquery', 'format_vsf/chartist', 'core/log'], function($, Chartist, log) {
 
-  "use strict"; // jshint ;_;
+    "use strict"; // jshint ;_;
 
-  log.debug('Progress Section Format Chartist AMD jQuery initialised');
+    log.debug('Progress Section Format Chartist AMD jQuery initialised');
 
-  return {
-    init: function(data) {
-      log.debug('Progress Section Format Chartist AMD init initialised');
-      log.debug(data);
+    /**
+     * Chartist.js plugin to pre fill donouts with animations
+     * author: moxx
+     * author-url: https://github.com/moxx/chartist-plugin-fill-donut
+     *
+     */
+    (function(window, document, Chartist) {
+        'use strict';
 
-      $(document).ready(function() {
-      // Create a simple bar chart
-      /*var data = {
-        labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri'],
-        series: [
-                  [5, 2, 4, 2, 0]
-                ]
-              }; */
-
-        var options = {
-          axisX: {
-            showLabel: true,
-            offset: 45,
-            labelOffset: {
-                x: 0,
-                y: 0
-            }
-          },
-          axisY: {
-            onlyInteger: true
-          },
-          chartPadding: {
-            top: 15,
-            right: 5,
-            bottom: 5,
-            left: 5
-          },
-          showPoint: true
+        var defaultOptions = {
+            fillClass: 'ct-fill-donut',
+            label : {
+                html: '<div></div>',
+                class: 'ct-fill-donut-label test'
+            },
+            items : [{}]
         };
-        new Chartist.Line('.ct-chart', data, options);
-      });
+
+        Chartist.plugins = Chartist.plugins || {};
+        Chartist.plugins.fillDonut = function(options) {
+            options = Chartist.extend({}, defaultOptions, options);
+            return function fillDonut(chart){
+                if(chart instanceof Chartist.Pie) {
+                    var $chart = $(chart.container);
+                    $chart.css('position', 'relative');
+                    var $svg;
+
+                    chart.on('draw', function(data) {
+                        if(data.type == 'slice'){
+                            if(data.index == 0)
+                                $svg = $chart.find('svg').eq(0);
+
+                            var $clone = $(data.group._node).clone();
+                            $clone.attr('class', $clone.attr('class') + ' ' + options.fillClass);
+
+                            $clone.find('path').each(function(){
+                                $(this).find('animate').remove();
+                                $(this).removeAttr('stroke-dashoffset');
+                            });
+
+                            $svg.prepend($clone);
+
+                        }
+                    });
+
+                    chart.on('created', function(data){
+                        var itemIndex = 0;
+
+                        if(chart.options.fillDonutOptions)
+                            options = Chartist.extend({}, options, chart.options.fillDonutOptions);
+
+                        $.each(options.items, function(){
+                            var $wrapper = $(options.label.html).addClass(options.label.class);
+                            var item = $.extend({}, {
+                                class : '',
+                                id: '',
+                                content : 'fillText',
+                                position: 'center', //bottom, top, left, right
+                                offsetY: 0, //top, bottom in px
+                                offsetX: 0 //left, right in px
+                            }, this);
+
+                            var content = $(item.content);
+
+                            if(item.id.length > 0)
+                                $wrapper.attr('id', item.id);
+                            if(item.class.length > 0)
+                                $wrapper.addClass('class', item.class);
+
+                            $chart.find('*[data-fill-index$="fdid-'+itemIndex+'"]').remove();
+                            $wrapper.attr('data-fill-index','fdid-'+itemIndex);
+                            itemIndex++;
+                        
+                            $wrapper.append(item.content).css({
+                                position : 'absolute'
+                            });
+
+                            $chart.append($wrapper);
+
+                            var cWidth = $chart.innerWidth() / 2;
+                            var cHeight = $chart.height() / 2;
+                            var wWidth = $wrapper.innerWidth() / 2;
+                            var wHeight = $wrapper.height() / 2;
+
+                            var style = {
+                                bottom : {
+                                    bottom : 0 + item.offsetY,
+                                    left: (cWidth - wWidth) + item.offsetX,
+                                },
+                                top : {
+                                    top : 0  + item.offsetY,
+                                    left: (cWidth - wWidth) + item.offsetX,
+                                },
+                                left : {
+                                    top : (cHeight - wHeight) + item.offsetY,
+                                    left: 0 + item.offsetX,
+                                },
+                                right : {
+                                    top : (cHeight - wHeight) + item.offsetY,
+                                    right: 0 + item.offsetX,
+                                },
+                                center : {
+                                    top : (cHeight - wHeight) + item.offsetY,
+                                    left: (cWidth - wWidth) + item.offsetX,
+                                }
+                            };
+
+                            $wrapper.css(style[item.position]);
+                        });
+                    });
+                }
+            }
+        }
+
+    }(window, document, Chartist));
+
+    return {
+        init: function(data) {
+            log.debug('Progress Section Format Chartist AMD init initialised');
+            log.debug(data);
+
+            $(document).ready(function() {
+                var data = {
+                    series: [60, 40],
+                    labels: ['', '']
+                };
+
+                var options = {
+                    donut: true,
+                    donutWidth: 40,
+                    startAngle: 0,
+                    total: 100,
+                    showLabel: false,
+                    plugins: [
+                        Chartist.plugins.fillDonut({
+                            items: [{
+                                content: '<i class="fa fa-tachometer"></i>',
+                                position: 'bottom',
+                                offsetY : 10,
+                                offsetX: -2
+                            }, {
+                                content: '<h3>60<span class="small">%</span></h3>'
+                            }]
+                        })
+                    ]
+                };
+                var chart = new Chartist.Pie('.ct-chart', data, options);
+
+                // Animation for the first series.
+                // Depreciated in Chrome - need to think about CSS: https://css-tricks.com/animating-svg-css/.
+                chart.on('draw', function(data) {
+                    if(data.type === 'slice' && data.index == 0) {
+                        // array animation.
+                        var pathLength = data.element._node.getTotalLength();
+
+                        data.element.attr({
+                            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+                        });
+
+                        var animationDefinition = {
+                            'stroke-dashoffset': {
+                                id: 'anim' + data.index,
+                                dur: 24000,
+                                from: -pathLength + 'px',
+                                to:  '0px',
+                                easing: Chartist.Svg.Easing.easeOutQuint,
+                                fill: 'freeze'
+                            }
+                        };
+                        data.element.attr({
+                            'stroke-dashoffset': -pathLength + 'px'
+                        });
+                        data.element.animate(animationDefinition, true);
+                    }
+                });
+            });
+        }
     }
-  }
- });
+});
 /* jshint ignore:end */
