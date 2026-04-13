84cbbdbe419e7da583e09ede7da066bdc8cc4782
LT&C <gemma@ltnc.nl>
2026-04-13 15:48:56 +0200

    LS-3517
    LS-3517 version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2026041301;
+$plugin->version = 202604130100;
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires = 2024100700.00; // 4.5 (Build: 20241007).  phpcs:ignore Squiz.PHP.CommentedOutCode.Found
 $plugin->supported = [405, 405];
