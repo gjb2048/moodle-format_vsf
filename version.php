30deca0149d3af932aa5b4b062d9345d6b4d6164
LT&C <gemma@ltnc.nl>
2026-04-13 15:07:53 +0200

    Update version.php
    version bump

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 202501150100;
+$plugin->version = 202504130100;
 $plugin->maturity = MATURITY_BETA;
 $plugin->requires = 2024100700.00; // 4.5 (Build: 20241007).  phpcs:ignore Squiz.PHP.CommentedOutCode.Found
 $plugin->supported = [405, 405];
